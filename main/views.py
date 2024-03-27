06d39c4d47dc161547237f0583e346ff3dd20342
Antoni <need.les324@gmail.com>
2024-03-27 20:27:49 +0300

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -1,16 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import context
 
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - HOME'
+        'title': 'MatroSkin - Главная',
+        'content': "MatroSkin"
+
     }
 
     return render(request, 'main/index.html', context)
     
 
 def about(request):
-    return HttpResponse('About Page')
+    context = {
+        'title': 'MatroSkin - О нас',
+        'content': "О нас",
+        'text_on_page': "Мы бренд одежды MatroSkin. Изготавливаем вышивку и печать объемных логотипов и изображений любой сложности и любого дизайна на футболках, худи, свитшотах. Вы можете предложить свой дизайн вышивки, цвет, размер, либо выбрать готовый вариант."
+    }
+
+    return render(request, 'main/about.html', context)
