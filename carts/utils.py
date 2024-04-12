a4d09f585f298ffd0e44db0a7373344ece60955a
Antoni <need.les324@gmail.com>
2024-04-12 11:29:47 +0300

    Create order temlate and form prepared

--- carts/utils.py
+++ carts/utils.py
@@ -6,5 +6,5 @@ def get_user_carts(request):
         return Cart.objects.filter(user=request.user)
     
     if not request.session.session_key:
-        request.seesion.create()
+        request.session.create()
     return Cart.objects.filter(session_key=request.session.session_key)
